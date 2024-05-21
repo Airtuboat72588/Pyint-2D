2fb84404708f40dfaa22841c82a6e7104c23e0c8
Airtuboat72588 <ian.guzman.c@gmail.com>
2024-05-21 03:34:35 +0000

    20 may 2024, 22:34

--- main.py
+++ main.py
@@ -3,4 +3,11 @@ from customtkinter import *
 app = CTk()
 app.title("Pyint-2D")
 
+def optionmenu_callback(choice):
+    print('optionmenu dropdown clicked:', choice)
+
+optionmenu_var = StringVar(value='option 2')
+optionmenu = CTkOptionMenu(app,values=['option 1', 'option 2'], width=140, height=28, command=optionmenu_callback, variable=optionmenu_var)
+optionmenu.place(x=10, y=10)
+
 app.mainloop()
