768fb5b078f6a33243e6503e5cab0f9848a3c29b
Airtuboat72588 <ian.guzman.c@gmail.com>
2024-05-21 03:05:29 +0000

    20 may 2024, 22:05

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 from customtkinter import * 
 
-root = CTk()
-root.title('Custom Tkinter')
+app = CTk()
+app.title("Pyint-2D")
 
-tabview = CTkTabview(root)
-tabview.place(x=10, y=10)
-
-tabview.add('tab 1')  # add tab at the end
-tabview.add('tab 2')  # add tab at the end
-tabview.set('tab 2')  # set currently visible tab
-
-button = CTkButton(master=tabview.tab('tab 1'))
-button.place(x=10, y=10)
-
-root.mainloop()
+app.mainloop()
