a38aeb38d05886ade3b0a361685247b9e84ee74c
Airtuboat72588 <ian.guzman.c@gmail.com>
2024-05-21 03:49:55 +0000

    20 may 2024, 22:49

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from customtkinter import *
 from tkinter import *
 
 root = Tk()
-app = CTk(root)
-app.title("Pyint-2D")
+root.title("Pyint-2D")
 root.state('zoomed')
 
-edición = CTkTabview(app)
-edición.place(x=10, y=10)
+edición = CTkTabview(root, width=600, height=600)
+edición.pack()
 
-edición.add('tab 1')  # add tab at the end
-edición.add('tab 2')  # add tab at the end
-edición.set('tab 1')  # set currently visible tab
+edición.add('Edición clásica')
+edición.add('Edición con números')
+edición.add('Edición con símbolos')
+edición.set('Edición clásica')
 
 
 root.mainloop()
