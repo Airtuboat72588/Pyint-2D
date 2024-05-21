90ab3f1e6189b6b00231ab33efc32239880adc3c
Airtuboat72588 <ian.guzman.c@gmail.com>
2024-05-21 02:20:49 +0000

    20 may 2024, 21:20

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from customtkinter import * 
 
 root = CTk()
+root.title('Custom Tkinter')
+
 Helloword = CTkLabel(root, text='Hello Word', width=40, height=28, fg_color='transparent')
 Helloword.place(x=10, y=10)
 
