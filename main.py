1301b47f14e666275032c4178f8c1ee73479c393
Airtuboat72588 <ian.guzman.c@gmail.com>
2024-05-26 21:29:32 +0000

    26 may 2024, 16:29

--- main.py
+++ main.py
@@ -18,7 +18,9 @@ edición.set('Edición clásica')
 color_actual = "black"
 
 def on_click(value):
-    print(f"Cuadro clickeado: {value}")
+    columna = value["column"]
+    fila = value["row"]
+    tabla_clásica.set_cell(fila, columna, color_actual)
 
 tabla_clásica = CTkTable(master = edición.tab("Edición clásica"),row=16, column=16, command=on_click)
 tabla_clásica.pack()
