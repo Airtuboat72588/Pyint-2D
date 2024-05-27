32053fe841fca257663ab295827db88b1e25bdcd
Airtuboat72588 <ian.guzman.c@gmail.com>
2024-05-27 00:26:42 -0600

    Se cambia a una clase para Tkinter

--- main.py
+++ main.py
@@ -1,81 +1,82 @@
-# Imports #
-
-# Importamos las clases necesarias
-from clase_editor import Editor
-
-# Importamos las librerías necesarias
-from customtkinter import * 
 from tkinter import *
 from CTkTable import *
+from customtkinter import *
+from clase_editor import Editor
 
-# Inicio #
+class root(Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Pyint-2D")
+        self.state('zoomed')
+        set_appearance_mode("light")
 
-# Menú #
-# Vamos a hacer el menu de nuestro proyecto para navegar entre funcionalidades #
+        self.matriz = [[0 for _ in range(16)] for _ in range(16)]
+        self.color_actual = "black"
+        self.símbolo_actual = None
+        self.número_actual = None
 
+        self.edición = CTkTabview(self, width=600, height=600, command=self.cambiar_a_símbolos)
+        self.edición.pack()
 
+        self.edición.add('Edición clásica')
+        self.edición.add('Edición con números')
+        self.edición.add('Edición con símbolos')
+        self.edición.set('Edición clásica')
 
-# Crear una Ventana Nueva de Tkinter #
-root = Tk()
-# Configuración de la Ventana #
-root.title("Pyint-2D")
-# Maximizar la Ventana #
-root.state('zoomed')
-# Cambiar el Fondo de la Ventana a brillante #
-set_appearance_mode("light")
+        self.tabla_clásica = CTkTable(master=self.edición.tab("Edición clásica"), row=16, column=16, command=self.cambiar_color, padx=2, pady=2, colors=["white", "white"], width=50, height=50, corner_radius=0)
+        self.tabla_clásica.pack()
 
-matriz = [[0 for _ in range(16)] for _ in range(16)]
+        self.tabla_símbolos = CTkTable(master=self.edición.tab("Edición con símbolos"), row=16, column=16, command=self.cambiar_símbolo, padx=2, pady=2, colors=["white", "white"], width=50, height=50, corner_radius=0)
+        self.tabla_símbolos.pack()
 
-def cambiar_a_símbolos():
-    global color_actual
-    global símbolo_actual
-    global número_actual
-    print(tabla_clásica.get())
-    if edición.get() == "Edición con símbolos":
-        if color_actual == "black":
-            símbolo_actual = "@"
-    elif edición.get() == "Edición clásica":
-        color_actual = "black"
-    elif edición.get() == "Edición con números":
-        color_actual = "black"
-        número_actual = "0"
+        self.tabla_números = CTkTable(master=self.edición.tab("Edición con números"), row=16, column=16, command=self.cambiar_número, padx=2, pady=2, colors=["white", "white"], width=50, height=50, corner_radius=0)
+        self.tabla_números.pack()
 
-edición = CTkTabview(root, width=600, height=600, command = cambiar_a_símbolos)
-edición.pack()
+    def set_appearance_mode(self, mode):
+        # Implementa la lógica para cambiar el modo de apariencia aquí
+        pass
 
-# Agregar pestañas al Tabview #
-edición.add('Edición clásica')
-edición.add('Edición con números')
-edición.add('Edición con símbolos')
-edición.set('Edición clásica')
+    def cambiar_a_símbolos(self):
+        if self.edición.get() == "Edición con símbolos":
+            for fila in range(16):
+                for columna in range(16):
+                    if self.matriz[fila][columna] == 1:
+                        self.tabla_símbolos.frame[fila, columna].configure(text="@")
+            self.símbolo_actual = "@"
+        elif self.edición.get() == "Edición clásica":
+            self.color_actual = "black"
+            for fila in range(16):
+                for columna in range(16):
+                    if self.matriz[fila][columna] == 1:
+                        self.tabla_clásica.insert(fila, columna, "", fg_color="black", bg_color="black")
+        elif self.edición.get() == "Edición con números":
+            self.número_actual = "0"
+            for fila in range(16):
+                for columna in range(16):
+                    if self.matriz[fila][columna] == 1:
+                        self.tabla_números.frame[fila, columna].configure(text="0")
 
-color_actual = "black"
+    def cambiar_color(self, value):
+        columna = value["column"]
+        fila = value["row"]
+        self.tabla_clásica.insert(fila, columna, 1, fg_color=self.color_actual, bg_color=self.color_actual)
+        self.matriz[fila][columna] = 1
+        print(self.matriz)
+        self.tabla_clásica.frame[fila, columna].configure(text="")
 
-def cambiar_color(value):
-    columna = value["column"]
-    fila = value["row"]
-    tabla_clásica.insert(fila, columna, 1, fg_color=color_actual, bg_color=color_actual)
-    matriz[fila][columna] = 1
-    print(matriz)
-    tabla_clásica.frame[fila, columna].configure(text="")
+    def cambiar_símbolo(self, value):
+        columna = value["column"]
+        fila = value["row"]
+        self.matriz[fila][columna] = 1
+        print(self.matriz)
+        self.tabla_símbolos.frame[fila, columna].configure(text="@")
 
-def cambiar_símbolo(value):
-    columna = value["column"]
-    fila = value["row"]
-    tabla_símbolos.frame[fila, columna].configure(text=símbolo_actual)
-    
-def cambiar_número(value):
-    columna = value["column"]
-    fila = value["row"]
-    tabla_números.frame[fila, columna].configure(text=número_actual)
+    def cambiar_número(self, value):
+        columna = value["column"]
+        fila = value["row"]
+        self.matriz[fila][columna] = 1
+        print(self.matriz)
+        self.tabla_números.frame[fila, columna].configure(text="1")
 
-tabla_clásica = CTkTable(master = edición.tab("Edición clásica"),row=16, column=16, command=cambiar_color, padx = 2, pady = 2, colors = ["white", "white"], width=50, height=50, corner_radius = 0)
-tabla_clásica.pack()
-
-tabla_símbolos = CTkTable(master = edición.tab("Edición con símbolos"),row=16, column=16, command=cambiar_símbolo, padx = 2, pady = 2, colors = ["white", "white"], width=50, height=50, corner_radius = 0)
-tabla_símbolos.pack()
-
-tabla_números = CTkTable(master = edición.tab("Edición con números"),row=16, column=16, command=cambiar_número, padx = 2, pady = 2, colors = ["white", "white"], width=50, height=50, corner_radius = 0)
-tabla_números.pack()
-
+root = root()
 root.mainloop()
